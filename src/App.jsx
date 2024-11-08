078a73359fb257374c861f1161258d8e08c3ca1c
shreyash-deshpande <deshpandeshreyash123@gmail.com>
2024-11-08 19:25:37 +0530

    refactor: refactor location to enhance modularity

--- src/App.jsx
+++ src/App.jsx
@@ -8,96 +8,11 @@ import HomePage from "./App/page/HomePage.jsx";
 import NotFoundPage from "./App/page/NotFoundPage";
 import ProductDetailPage from "./App/page/ProductDetailPage";
 import GalleryPage from "./App/page/GalleryPage";
+import {gallery} from "./assets/json/gallery/gallery";
 
 export const LanguageContext = createContext(null);
 
 export function App() {
-    const gallery = {
-        newLaunch: [
-            {
-                path: 'new-launch/2023',
-                total: 2,
-            },
-            {
-                path: 'new-launch/2024',
-                total: 5,
-            },
-        ],
-        companyNews: [
-            {
-                path: 'company-news',
-                total: 13,
-            },
-        ],
-        annualDealersMeet: [
-            {
-                path: 'annual-dealers-meet/sambhajinagar-2024',
-                total: 15,
-            },
-            {
-                path: 'annual-dealers-meet/sambhajinagar-2023',
-                total: 8,
-            },
-            {
-                path: 'annual-dealers-meet/goa',
-                total: 9,
-            },
-            {
-                path: 'annual-dealers-meet/sambhajinagar-2022',
-                total: 8,
-            },
-            {
-                path: 'annual-dealers-meet/wardha-2017',
-                total: 4,
-            },
-            {
-                path: 'annual-dealers-meet/kerala',
-                total: 8,
-            },
-            {
-                path: 'annual-dealers-meet/wardha-2014',
-                total: 9,
-            },
-        ],
-        fieldVisits: [
-            {
-                path: 'field-visits',
-                total: 30,
-            },
-        ],
-        tour: [
-            {
-                path: 'tour/thailand-2024',
-                total: 5,
-            },
-            {
-                path: 'tour/kashmir',
-                total: 2,
-            },
-            {
-                path: 'tour/diu',
-                total: 4,
-            },
-            {
-                path: 'tour/thailand-2023',
-                total: 5,
-            },
-            {
-                path: 'tour/singapore',
-                total: 14,
-            },
-            {
-                path: 'tour/dwarka-2017',
-                total: 7,
-            },
-        ],
-        farmerMeeting: [
-            {
-                path: 'farmer-meeting',
-                total: 22,
-            },
-        ],
-    };
     const router = createBrowserRouter([
         {
             path: "/",
---
+++ src/assets/json/gallery/gallery.js
@@ -0,0 +1,86 @@
+export const gallery = {
+    newLaunch: [
+        {
+            path: 'new-launch/2023',
+            total: 2,
+        },
+        {
+            path: 'new-launch/2024',
+            total: 5,
+        },
+    ],
+    companyNews: [
+        {
+            path: 'company-news',
+            total: 13,
+        },
+    ],
+    annualDealersMeet: [
+        {
+            path: 'annual-dealers-meet/sambhajinagar-2024',
+            total: 15,
+        },
+        {
+            path: 'annual-dealers-meet/sambhajinagar-2023',
+            total: 8,
+        },
+        {
+            path: 'annual-dealers-meet/goa',
+            total: 9,
+        },
+        {
+            path: 'annual-dealers-meet/sambhajinagar-2022',
+            total: 8,
+        },
+        {
+            path: 'annual-dealers-meet/wardha-2017',
+            total: 4,
+        },
+        {
+            path: 'annual-dealers-meet/kerala',
+            total: 8,
+        },
+        {
+            path: 'annual-dealers-meet/wardha-2014',
+            total: 9,
+        },
+    ],
+    fieldVisits: [
+        {
+            path: 'field-visits',
+            total: 30,
+        },
+    ],
+    tour: [
+        {
+            path: 'tour/thailand-2024',
+            total: 5,
+        },
+        {
+            path: 'tour/kashmir',
+            total: 2,
+        },
+        {
+            path: 'tour/diu',
+            total: 4,
+        },
+        {
+            path: 'tour/thailand-2023',
+            total: 5,
+        },
+        {
+            path: 'tour/singapore',
+            total: 14,
+        },
+        {
+            path: 'tour/dwarka-2017',
+            total: 7,
+        },
+    ],
+    farmerMeeting: [
+        {
+            path: 'farmer-meeting',
+            total: 22,
+        },
+    ],
+};
